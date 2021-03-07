beff1c873715c5ed65efe1b96315cabed17c602a
minsunki <minsunki@student.42seoul.kr>
2021-03-08 01:06:28 +0900

    eod

--- get_next_line.c
+++ get_next_line.c
@@ -6,14 +6,59 @@
 /*   By: minsunki <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/07 23:20:08 by minsunki          #+#    #+#             */
-/*   Updated: 2021/03/07 23:58:20 by minsunki         ###   ########.fr       */
+/*   Updated: 2021/03/08 01:06:01 by minsunki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int		get_next_line(int fd, char **line)
+static int	find_nl(const char *str)
 {
+	size_t		i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '\n')
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+int			get_next_line(int fd, char **line)
+{
+	static char	*dat;
+	char		*tmp;
+	char		buf[BUFFER_SIZE + 1];
+	ssize_t		rbytes;
+	int			nidx;
+
 	if (fd < 0 || !line)
+		return (-1);
+	while ((rbytes = read(fd, buf, BUFFER_SIZE)) > 0)
+	{
+		buf[rbytes] = '\0';
+		tmp = dat;
+		dat = ft_strjoin(dat, buf);
+		free(tmp);
+		if ((nidx = find_nl(buf)) != -1)
+			return (ft_substr(buf, 0, nidx));
+	}
+
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*ret;
+
+	if (!s)
 		return (0);
+	if ((unsigned int)ft_strlen(s) < start)
+		return (ft_strdup(""));
+	len = (ft_strlen(s + start) < len ? ft_strlen(s + start) : len);
+	if (!(ret = (char *)malloc(sizeof(char) * (len + 1))))
+		return (0);
+	ft_strlcpy(ret, s + start, len + 1);
+	return (ret);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: minsunki <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/07 23:20:30 by minsunki          #+#    #+#             */
-/*   Updated: 2021/03/07 23:53:49 by minsunki         ###   ########.fr       */
+/*   Updated: 2021/03/08 00:26:46 by minsunki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@
 int		get_next_line(int fd, char **line);
 char	*ft_strjoin(char const *s1, char const s2);
 char	*ft_strdup(const char *str);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 size_t	*ft_strlcpy(char *dest, const char *src, size_t size);
 size_t	ft_strlcat(char *dest, const char *src, size_t size);
 size_t	ft_strlen(char *s)
