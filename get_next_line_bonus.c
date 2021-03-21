44c0544532451f3044e4d5a814422e528671a0cb
minsunki <minsunki@student.42seoul.kr>
2021-03-21 20:43:23 +0900

    add bonus

---
+++ get_next_line_bonus.c
@@ -0,0 +1,103 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: minsunki <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/07 23:20:08 by minsunki          #+#    #+#             */
+/*   Updated: 2021/03/21 20:41:43 by minsunki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+static int	find_nl(const char *str)
+{
+	size_t		i;
+
+	i = 0;
+	if (!str)
+		return (-1);
+	while (str[i])
+	{
+		if (str[i] == '\n')
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+static int	get_line(char **dat, char **ret)
+{
+	int		nidx;
+	char	*tmp;
+
+	if ((nidx = find_nl(*dat)) != -1)
+	{
+		*ret = ft_substr(*dat, 0, nidx);
+		tmp = *dat;
+		*dat = ft_substr(*dat, nidx + 1, -1);
+		free(tmp);
+		if (!(**dat))
+		{
+			free(*dat);
+			*dat = 0;
+			return (1);
+		}
+		return (1);
+	}
+	return (0);
+}
+
+static int	gnl_eof(char **dat, char **ret)
+{
+	if (*dat && get_line(dat, ret))
+		return (1);
+	else if (*dat)
+	{
+		*ret = *dat;
+		*dat = 0;
+		return (0);
+	}
+	*ret = ft_strdup("");
+	return (0);
+}
+
+int			get_next_line(int fd, char **line)
+{
+	static char	*dat[FD_LIMIT];
+	char		*tmp;
+	char		buf[BUFFER_SIZE + 1];
+	ssize_t		rbytes;
+
+	if (fd < 0 || !line || BUFFER_SIZE <= 0)
+		return (-1);
+	while ((rbytes = read(fd, buf, BUFFER_SIZE)) > 0)
+	{
+		buf[rbytes] = '\0';
+		tmp = dat[fd];
+		dat[fd] = ft_strjoin(dat[fd], buf);
+		(tmp ? free(tmp) : tmp);
+		if (get_line(&dat[fd], line))
+			return (1);
+	}
+	if (rbytes < 0)
+		return (-1);
+	return (gnl_eof(&dat[fd], line));
+}
+
+char		*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*ret;
+
+	if (!s)
+		return (0);
+	if ((unsigned int)ft_strlen(s) < start)
+		return (ft_strdup(""));
+	len = (ft_strlen(s + start) < len ? ft_strlen(s + start) : len);
+	if (!(ret = (char *)malloc(sizeof(char) * (len + 1))))
+		return (0);
+	ft_strlcpy(ret, s + start, len + 1);
+	return (ret);
+}
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: minsunki <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/07 23:21:10 by minsunki          #+#    #+#             */
+/*   Updated: 2021/03/14 20:54:43 by minsunki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	ret;
+
+	ret = 0;
+	while (*str++)
+		ret++;
+	return (ret);
+}
+
+char	*ft_strdup(const char *str)
+{
+	char	*ret;
+	size_t	slen;
+
+	if (!str)
+		return (0);
+	slen = ft_strlen(str);
+	if (!(ret = (char *)malloc(sizeof(char) * (slen + 1))))
+		return (0);
+	ft_strlcpy(ret, str, slen + 1);
+	return (ret);
+}
+
+size_t	ft_strlcpy(char *dest, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	slen;
+
+	if (!dest || !src)
+		return (0);
+	slen = ft_strlen(src);
+	i = 0;
+	if (!size)
+		return (slen);
+	while (src[i] && i < (size - 1))
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (slen);
+}
+
+size_t	ft_strlcat(char *dest, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	dlen;
+	size_t	slen;
+
+	if (!dest || !src)
+		return (0);
+	dlen = ft_strlen(dest);
+	slen = ft_strlen(src);
+	i = 0;
+	if (size < dlen + 1)
+		return (size + slen);
+	if (size > dlen + 1)
+	{
+		while (src[i] && dlen + 1 + i < size)
+		{
+			dest[dlen + i] = src[i];
+			i++;
+		}
+	}
+	dest[dlen + i] = '\0';
+	return (dlen + slen);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*ret;
+	size_t	s1_len;
+	size_t	s2_len;
+
+	if (!s1 && !s2)
+		return (0);
+	else if (!s1 || !s2)
+		return (!s1 ? ft_strdup(s2) : ft_strdup(s1));
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	if (!(ret = (char *)malloc(sizeof(char) * (s1_len + s2_len + 1))))
+		return (0);
+	ft_strlcpy(ret, s1, s1_len + 1);
+	ft_strlcat(ret + s1_len, s2, s2_len + 1);
+	return (ret);
+}
